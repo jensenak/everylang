3cc06b04bf1cb93a566396fedc8d98f7ee936318
Adam Jensen <jensen.ak@gmail.com>
2015-12-04 16:18:53 -0700

    Bash 'objects' done

---
+++ bash/everylang.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+# The following functions define a behavior that... 'resembles' object-oriented programming.
+
+# new "constructs" an object. It actually just builds arrays of methods and  attributes in a pseudo-namespace.
+new() {
+    declare -g obj_${2}_methods
+    methods=obj_${2}_methods
+    declare -gA obj_${2}_attrs
+    attrs=obj_${2}_attrs
+
+    eval "$attrs=$(${1} attrs)"
+    eval "$methods=$(${1} methods)"
+}
+
+# call is used to execute a method on an "object"
+call() {
+    varname=obj_${1}_methods[$2]
+    eval ${!varname}
+}
+
+# retrieve an attribute of an "object"
+attrget() {
+    varname=obj_${1}_attrs[$2]
+    echo ${!varname}
+}
+
+# set an attribute of an "object"
+attrset() {
+    varname=obj_${1}_attrs[$2]
+    eval '[ ${'$varname'+exists} ]'
+    if [[ $? -eq 0 ]]
+    then
+        eval ${varname}=$3
+    fi
+}
+
+############################################
+# The objects and methods for this program #
+############################################
+
+sayname() {
+    attrget bob name
+}
+
+person() {
+    if [[ "${1}x" == "attrsx" ]]
+    then
+        echo "([name]=unset [age]=unset [type]=person)"
+    fi
+
+    if [[ "${1}x" == "methodsx" ]]
+    then
+        echo "([sayname]=sayname)"
+    fi
+}
+
+echo "Start"
+new person bob
+attrset bob name Bob
+attrset bob field engineering
+call bob sayname
+attrget bob field
+echo ${obj_bob_attrs[@]}
+echo "Done"
