3703841018cd6ae3bb9230cd476b4fb9693c056f
Adam Jensen <jensen.ak@gmail.com>
2015-12-11 16:27:31 -0700

    Semi-working model... needs some love

---
+++ python/everylang.py
@@ -0,0 +1,57 @@
+from random import randint
+import time
+
+class person():
+    count = 0
+
+    def __init__(self, name, age, fitness):
+        self.wins = 0
+        self.it = 0
+        self.name = name
+        self.speed = int(age)*int(fitness)
+        person.count += 1
+
+    def run(self):
+       return randint(0, self.speed)
+
+    def updateStats(self, winner):
+        if winner:
+            self.wins += 1
+        self.it += 1
+
+    def getStats(self):
+        return "{} was a goose {} times, and won {} times".format(self.name, self.it, self.wins)
+
+
+players = []
+while True:
+    name = input("Enter player name (empty to continue): ")
+    age = input("What is this player's age: ")
+    fit = input("1 = Active, 2 = Average, 3 = Sedentary; What is this player's fitness level? ")
+    try:
+        players.append(person(name, age, fit))
+    except ValueError:
+        print("All players registered, moving to game play!")
+        break
+
+rounds = input("How many rounds do you want to play? ")
+
+goose = randint(0, person.count)
+stats = {}
+for r in rounds: 
+    newGoose = randint(0, person.count-1)
+    print("{} is it, and chose {} as the goose".format(players[goose].name, 
+                                                       players[newGoose].name))
+    if players[goose].run() > players[newGoose].run():
+        print(" --> {} wins".format(players[newGoose].name))
+        players[newGoose].updateStats(True)
+        players[goose].updateStats(False)
+    else:
+        print(" --> {} wins".format(players[goose].name))
+        players[newGoose].updateStats(False)
+        players[goose].updateStats(True)
+        goose = newGoose
+
+print("Final stats: ")
+for p in players:
+    print(p.getStats())
