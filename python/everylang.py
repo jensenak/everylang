28bc78ba202eebd3cc6aa80adadcef381e4be399
Adam Jensen <jensen.ak@gmail.com>
2015-12-18 12:53:42 -0700

    Python done

--- python/everylang.py
+++ python/everylang.py
@@ -8,7 +8,7 @@ class person():
         self.wins = 0
         self.it = 0
         self.name = name
-        self.speed = int(age)*int(fitness)
+        self.speed = int(int(age)*(.8+int(fitness)/5))
         person.count += 1
 
     def run(self):
@@ -17,15 +17,19 @@ class person():
     def updateStats(self, winner):
         if winner:
             self.wins += 1
-        self.it += 1
+        else:
+            self.it += 1
 
     def getStats(self):
-        return "{} was a goose {} times, and won {} times".format(self.name, self.it, self.wins)
+        return "{} was it {} times, and won {} times".format(self.name, self.it, self.wins)
 
 
 players = []
 while True:
     name = input("Enter player name (empty to continue): ")
+    if name == "":
+        print("All players registered, moving to game play!")
+        break
     age = input("What is this player's age: ")
     fit = input("1 = Active, 2 = Average, 3 = Sedentary; What is this player's fitness level? ")
     try:
@@ -34,15 +38,20 @@ while True:
         print("All players registered, moving to game play!")
         break
 
-rounds = input("How many rounds do you want to play? ")
+rounds = int(input("How many rounds do you want to play? "))
 
 goose = randint(0, person.count)
+players[goose].updateStats(False)
 stats = {}
-for r in rounds: 
+for r in range(rounds):
     newGoose = randint(0, person.count-1)
+    while newGoose == goose:
+        newGoose = randint(0, person.count-1)
     print("{} is it, and chose {} as the goose".format(players[goose].name, 
                                                        players[newGoose].name))
-    if players[goose].run() > players[newGoose].run():
+    g = players[goose].run()
+    n = players[newGoose].run()
+    if g > n:
         print(" --> {} wins".format(players[newGoose].name))
         players[newGoose].updateStats(True)
         players[goose].updateStats(False)
